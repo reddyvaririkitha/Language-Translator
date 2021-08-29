27a79505d2f0d600b2a2c1dfed6cb453f9d5b5eb
Reddyvari Rikitha <73999899+reddyvaririkitha@users.noreply.github.com>
2021-08-29 14:34:29 +0530

    Update and rename Language_Tanslator.py to Language_Translator.py

--- Language_Translator.py
+++ Language_Translator.py
@@ -1,14 +1,13 @@
- #importing modules
 from tkinter import *
 from translate import Translator
-# initializing window
+
 Screen = Tk()
 Screen.title("Language Translator")
 
 InputLanguageChoice = StringVar()
 TranslateLanguageChoice = StringVar()
 
-#tuple for choosing languages
+
 LanguageChoices = {'Hindi','English','French','German','Spanish'}
 InputLanguageChoice.set('English')
 TranslateLanguageChoice.set('Hindi')
@@ -18,12 +17,12 @@ def Translate():
     Translation = translator.translate(TextVar.get())
     OutputVar.set(Translation)
 
-#choice for input language
+
 InputLanguageChoiceMenu = OptionMenu(Screen,InputLanguageChoice,*LanguageChoices)
 Label(Screen,text="Choose a Language").grid(row=0,column=1)
 InputLanguageChoiceMenu.grid(row=1,column=1)
 
-#choice in which the language is to be translated
+
 NewLanguageChoiceMenu = OptionMenu(Screen,TranslateLanguageChoice,*LanguageChoices)
 Label(Screen,text="Translated Language").grid(row=0,column=2)
 NewLanguageChoiceMenu.grid(row=1,column=2)
@@ -36,7 +35,9 @@ Label(Screen,text="Output Text").grid(row=2,column =2)
 OutputVar = StringVar()
 TextBox = Entry(Screen,textvariable=OutputVar).grid(row=2,column = 3)
 
-#Button for calling function
+
 B = Button(Screen,text="Translate",command=Translate, relief = GROOVE).grid(row=3,column=1,columnspan = 3)
 
 mainloop()
+
+#Also trying to include language detection and website instead of tkinter
